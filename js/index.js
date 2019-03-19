0074dd853d805a23ae1f6fd530283f6b7cdff18c
Endre Szabó <endre1988@freemail.hu>
2019-03-19 18:37:30 +0100

    OK

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-//A legújabb openlayers formátumába át kéne írni a js-t.
-
-//A proj műveletet egy zárt függvény csinálja.
-
-// koordinátát írja ki az egér mozgatásra.
-
-
-
 var myProjectionName = "EPSG:23700";
 proj4.defs(
 myProjectionName,
@@ -35,18 +27,15 @@ var osmLayer = new ol.layer.Tile({
 
 //A gombra kattintás során elindul az átalakítás a beírt Polygon((x1 y1, x2 y2, x3 y3,....)) érték alapján
 document.getElementById('gomb').addEventListener("click", alakito) /*   var value =  
-  //Javítva getelementbyID-ra és addeventlistenerrel megoldva natív JS
+  //Javítva getelementbyID-ra és addeventlistenerre natív JS
 /*--------------------------------------------------------------------------------*/
 
 $("#uname").on("change keyup paste", function() {
 });
 $("#refresh").on("click", function() {
-   /*var elem = document.getElementById('map');
- elem.parentNode.removeChild(elem);*/
    window.location.reload(true);
 });
 
-//$('[data-toggle="tooltip"]').tooltip().animate(fadeIn(1500));  //bootstrap tooltip js
 $(".map").on('mousemove', function (e){
             str="Északi-szélesség: "+e.latlng.lat.toFixed(5)+" Keleti-hosszúság: "+e.latlng.lng.toFixed(5)+" Nagyítás szint: "+mymap.getZoom();
           $("#coord").html(str);
@@ -60,7 +49,6 @@ function (data) {
   $select.html('');
     var result = data.features;
         for (var i = 0; i < result.length; i++) {
-           //console.log(result[i].properties);
            $select.append('<option id="'+result[i].properties.GEOM+ '">' +result[i].properties.NAME+'</option>');
           }
       });
@@ -76,13 +64,11 @@ $.getJSON(url,//     https://api.myjson.com/bins/15sl2k     abc sorrendű json (
    var geom="";
     var result = data.features;
         for (var i = 0; i < result.length; i++) {
-           //console.log(result[i].properties);
            if (ch===result[i].properties.NAME){
            geom += result[i].properties.GEOM;}
           }
    document.getElementById("uname").value=geom;
    alakito();
-   //$("#convert").html(geom);
       });
 }
 
@@ -109,14 +95,12 @@ function alakito(){
    console.log(z);
    var str = s.substring(9, s.length - 2);
    var coord1 = str.split(",");
-   // for ciklus szétszedés
    for (var i = 0; i < coord1.length; i++) {
       coord1[i] = coord1[i].split(" ");
       console.log(coord1[i]); //új
    }
    var text = "";
    $("#convert").html(z);
-   //POLYGON((493707.2 251150.2,493654 250833.8,493967.6 250822.6,494012.4 251060.6,493707.2 251150.2)) teszt WKT
    var ossz;
    var ossz1;
    var kis="";
@@ -127,8 +111,6 @@ function alakito(){
       console.log(nagy.substring(0, nagy.length-1));
       kis+= parseInt(coord1[j][1])+" ";
       console.log(kis.substring(0, kis.length-1));
-        // text += coord1[i] + ","
-      //text=array.push(coord1[i]);
       var p = new proj4.Point(
          parseFloat(coord1[j][0]),
          parseFloat(coord1[j][1])
@@ -195,32 +177,3 @@ function alakito(){
    map.addLayer(vector);
    map.setView(view2);
 }
-
-
-/*
-   var source2 = new VectorSource({wrapX: false});
-   var vector2 = new VectorLayer({
-        source: source2
-      });
-   
-      var typeSelect = document.getElementById('type');
-      var draw; // global so we can remove it later
-      function addInteraction() {
-        var value = typeSelect.value;
-        if (value !== 'None') {
-          draw = new Draw({
-            source: source2,
-            type: typeSelect.value
-          });
-          map.addInteraction(draw);
-        }
-      }
-      /**
-       * Handle change event.
-       
-      typeSelect.onchange = function() {
-        map.removeInteraction(draw);
-        addInteraction();
-      };
-      addInteraction();
-*/
