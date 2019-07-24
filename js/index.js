2da77d30e8b3053eed95108de4dca57b8dbaa45b
Endi <endre1988@freemail.hu>
2019-07-24 09:52:02 +0200

    Update
    Animated element

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+//A legújabb openlayers formátumába át kéne írni a js-t.
+
+//A proj műveletet egy zárt függvény csinálja.
+
+// koordinátát írja ki az egér mozgatásra.
+
+
+
 var myProjectionName = "EPSG:23700";
 proj4.defs(
 myProjectionName,
@@ -27,15 +35,19 @@ var osmLayer = new ol.layer.Tile({
 
 //A gombra kattintás során elindul az átalakítás a beírt Polygon((x1 y1, x2 y2, x3 y3,....)) érték alapján
 document.getElementById('gomb').addEventListener("click", alakito) /*   var value =  
-  //Javítva getelementbyID-ra és addeventlistenerre natív JS
+  //Javítva getelementbyID-ra és addeventlistenerrel megoldva natív JS
+
 /*--------------------------------------------------------------------------------*/
 
 $("#uname").on("change keyup paste", function() {
 });
 $("#refresh").on("click", function() {
+   /*var elem = document.getElementById('map');
+ elem.parentNode.removeChild(elem);*/
    window.location.reload(true);
 });
 
+//$('[data-toggle="tooltip"]').tooltip().animate(fadeIn(1500));  //bootstrap tooltip js
 $(".map").on('mousemove', function (e){
             str="Északi-szélesség: "+e.latlng.lat.toFixed(5)+" Keleti-hosszúság: "+e.latlng.lng.toFixed(5)+" Nagyítás szint: "+mymap.getZoom();
           $("#coord").html(str);
@@ -49,6 +61,7 @@ function (data) {
   $select.html('');
     var result = data.features;
         for (var i = 0; i < result.length; i++) {
+           //console.log(result[i].properties);
            $select.append('<option id="'+result[i].properties.GEOM+ '">' +result[i].properties.NAME+'</option>');
           }
       });
@@ -64,11 +77,13 @@ $.getJSON(url,//     https://api.myjson.com/bins/15sl2k     abc sorrendű json (
    var geom="";
     var result = data.features;
         for (var i = 0; i < result.length; i++) {
+           //console.log(result[i].properties);
            if (ch===result[i].properties.NAME){
            geom += result[i].properties.GEOM;}
           }
    document.getElementById("uname").value=geom;
    alakito();
+   //$("#convert").html(geom);
       });
 }
 
@@ -95,12 +110,14 @@ function alakito(){
    console.log(z);
    var str = s.substring(9, s.length - 2);
    var coord1 = str.split(",");
+   // for ciklus szétszedés
    for (var i = 0; i < coord1.length; i++) {
       coord1[i] = coord1[i].split(" ");
       console.log(coord1[i]); //új
    }
    var text = "";
    $("#convert").html(z);
+   //POLYGON((493707.2 251150.2,493654 250833.8,493967.6 250822.6,494012.4 251060.6,493707.2 251150.2)) teszt WKT
    var ossz;
    var ossz1;
    var kis="";
@@ -111,6 +128,8 @@ function alakito(){
       console.log(nagy.substring(0, nagy.length-1));
       kis+= parseInt(coord1[j][1])+" ";
       console.log(kis.substring(0, kis.length-1));
+        // text += coord1[i] + ","
+      //text=array.push(coord1[i]);
       var p = new proj4.Point(
          parseFloat(coord1[j][0]),
          parseFloat(coord1[j][1])
@@ -171,9 +190,105 @@ function alakito(){
    });
      var view2= new ol.View({
           center: hunweb,
-          zoom: 14
+          zoom: 12
      });
    
    map.addLayer(vector);
    map.setView(view2);
 }
+
+
+/*
+   var source2 = new VectorSource({wrapX: false});
+   var vector2 = new VectorLayer({
+        source: source2
+      });
+   
+      var typeSelect = document.getElementById('type');
+
+      var draw; // global so we can remove it later
+      function addInteraction() {
+        var value = typeSelect.value;
+        if (value !== 'None') {
+          draw = new Draw({
+            source: source2,
+            type: typeSelect.value
+          });
+          map.addInteraction(draw);
+        }
+      }
+
+
+      /**
+       * Handle change event.
+       
+      typeSelect.onchange = function() {
+        map.removeInteraction(draw);
+        addInteraction();
+      };
+
+      addInteraction();
+*/
+/*Átírni sima js-be addeventlistenerekkel*/
+
+
+$( document ).ready(function() {
+
+
+/*$("#gombok").css({"display":"block","right":"150px"});*/
+
+$("#toleft").on('click',function(){
+   if($(this).attr('data-click-state') == 1) {
+      $(this).attr('data-click-state', 0)
+      $(".filter").animate({"right":"0px"},{duration:1000})
+      $("#searching").animate({"right":"160px"},{duration:1000});
+   }
+   else {
+   $(this).attr('data-click-state', 1)
+      $(".filter").animate({"right":"-165px"},{duration:1000})
+      $("#searching").animate({"right":"0px"},{duration:1000})
+      $("#filter").css({"display":"none"});
+      $("#telepulesek").css({"display":"none"});
+      $('#search').val('');
+   }
+});
+   
+$("#toleft2").on('click',function(){
+   
+   if($(this).attr('data-click-state') == 1) {
+      $(this).attr('data-click-state', 0)
+      $("#EOV").animate({"right":"-450px"},{duration:1000})
+      $("#gombok").animate({"right":"-450px"},{duration:1000})
+      $("#e-k").animate({"right":"-450px"},{duration:1000})
+      $("#EOV_slide").animate({"right":"0px"},{duration:1000});
+   }
+   else {
+      $(this).attr('data-click-state', 1)
+      $("#EOV").animate({"right":"0px"},{duration:1000})
+      $("#e-k").animate({"right":"0px"},{duration:1000})
+      $("#gombok").animate({"right":"75px"},{duration:1000})
+      $("#EOV_slide").animate({"right":"280px"},{duration:1000})
+   }
+});
+   
+$("#search").on('change keyup paste',function(){
+   $("#valogat").css({"display":"block"});
+/*$("#gombok").css({"display":"block","right":"150px"});*/
+});
+
+   
+$("#search").on('change keyup paste',function(){
+   $("#valogat").css({"display":"block"});
+   $("#telepulesek").css({"display":"block"});
+    
+});
+$("#telepulesek").on('change keyup paste',function(){
+   $("#filter").css({"display":"block"});
+});   
+   
+   
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+   
+});
